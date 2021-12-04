762a9f683f32cb8f29c5bf6bada346f446471f73
NIDHIRAGHU <raghavendra.n@northeastern.edu>
2021-12-04 17:23:48 -0500

    Cart Edit | Remove | Redundancy orderitem

--- DoorDeliveryApplication/src/UI/Customer/CustomerCartJPanel.java
+++ DoorDeliveryApplication/src/UI/Customer/CustomerCartJPanel.java
@@ -8,7 +8,10 @@ package ui.Customer;
 import Business.Customer.Customer;
 import Business.Ecosystem;
 import Business.Network.Network;
+import Business.Orders.Cart;
+import Business.Orders.Order;
 import Business.Orders.OrderItem;
+import UI.Alert;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
@@ -21,15 +24,18 @@ public class CustomerCartJPanel extends javax.swing.JPanel {
     /**
      * Creates new form CustomerCartJPanel
      */
-    
     Customer customer;
-        DefaultTableModel tableModel;
+    DefaultTableModel tableModel;
+    Alert alert;
+    Order order;
 
     public CustomerCartJPanel(Ecosystem system, Network network, Customer customer) {
         initComponents();
-        
+
         this.customer = customer;
         this.tableModel = (DefaultTableModel) cartTable.getModel();
+        this.order = new Order();
+        this.alert = new Alert();
         this.setVisible(true);
         populateTable();
     }
@@ -47,7 +53,7 @@ public class CustomerCartJPanel extends javax.swing.JPanel {
         cartTable = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        labelTotal = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
 
@@ -81,23 +87,95 @@ public class CustomerCartJPanel extends javax.swing.JPanel {
         add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 120, -1, 133));
 
         jButton1.setText("FETCH CART");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 70, -1, -1));
 
         jLabel1.setText("ORDER TOTAL:");
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 300, -1, -1));
 
-        jLabel2.setText("<value>");
-        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 300, -1, -1));
+        labelTotal.setText("<value>");
+        add(labelTotal, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 300, -1, -1));
 
         jButton2.setText("PLACE ORDER");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 340, -1, -1));
 
         jButton3.setText("EDIT CART");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 260, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+//        EDIT CART
+        int rows = tableModel.getRowCount();
+        Cart custoemrcart = this.customer.getCustomerCart();
+
+        ArrayList<OrderItem> customerCartItems = this.customer.getCustomerCart().getCartItems();
+
+        try {
+            for (int i = 0; i < rows; i++) {
+                if ((Boolean) tableModel.getValueAt(i, 4)) {
+//                    traverse through his cartitems and delete orderitem
+                    for (OrderItem o : customerCartItems) {
+                        if (o.getProductId() == Integer.valueOf((Integer) tableModel.getValueAt(i, 0))) {
+//                            Modify cart
+                            customerCartItems.remove(o);
+                            this.alert.ShowAlert("Removed form cart!");
+                        }
+                    }
+
+                } else {
+                    for (OrderItem oi : customerCartItems) {
+                        if (oi.getProductName().equals(String.valueOf((String) tableModel.getValueAt(i, 1)))) {
+                            oi.setQty(Integer.valueOf((Integer) tableModel.getValueAt(i, 3)));
+                        }
+                    }
+                }
+            }
+
+//            Set all items ordered to the order 
+            this.order.setItemsOrdered(customerCartItems);
+            this.order.calcOrderTotal();
+            labelTotal.setText("$ " + String.valueOf(this.order.getPrice()));
+
+        } catch (Exception e) {
+            System.out.println(e + " CART PROBLEM IN VACCINE");
+        }
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+
+        populateTable();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+
+        ArrayList<Order> customerOrder = this.customer.getOrderlist();
+        customerOrder.add(order);
+        
+        this.alert.ShowAlert("Order placed successfully!");
+        this.customer.getCustomerCart().setCartItems(new ArrayList<OrderItem>());
+        populateTable();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     public void populateTable() {
-         tableModel.setRowCount(0);
+        tableModel.setRowCount(0);
 
         ArrayList<OrderItem> cartOrder = this.customer.getCustomerCart().getCartItems();
         try {
@@ -121,7 +199,7 @@ public class CustomerCartJPanel extends javax.swing.JPanel {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel labelTotal;
     // End of variables declaration//GEN-END:variables
 }
