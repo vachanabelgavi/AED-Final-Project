f0f810e18ea142de369eafe5ad02ea9d168a2804
PannagaVeeramohan <91296338+PannagaVeeramohan@users.noreply.github.com>
2021-12-04 11:24:36 -0500

    Updating enterprise UI (#8)
    * Updating network2
    
    * Updating Enterprise role
    
    * Updating

--- DoorDeliveryApplication/src/Business/ConfigureASystem.java
+++ DoorDeliveryApplication/src/Business/ConfigureASystem.java
@@ -153,17 +153,59 @@ public class ConfigureASystem {
 
         n.setEnterpriseDirectory(edir);
 
+      //Creating 2nd network
+        
+        Network n2 = system.createNetwork();
+        n2.setNetworkName("Massachusettes");
+        
+        
+        CustomerDirectory cdri2 = n2.getCustomerDirectory();
+        Customer myCustomernet2 = cdri2.createCustomer("customer 1 in network2", "cus1", "cus123", "hjj", 0, "Boston addr", "addd", 23);
+        
+        //  ONE ENTERPRISE
+        EnterpriseDirectory edirnetwork2 = new EnterpriseDirectory();
+        Enterprise enet2 = edirnetwork2.createEnterprise(Enterprise.EnterpriseType.Pharmaceutical, "Local Pharma Enterprise", "Massachusettes");
+       // System.out.println(edirnetwork2.getEnterpriseList());
+        
+        
+        OrganizationDirectory onet2 = enet2.getOrganizationDirectory();
+
+        Organization net2org1 = onet2.createOrganization(Organization.Type.Doctor, "Doctor Associate", "Massachusettes state", 2120);
+        Organization net2org2 = onet2.createOrganization(Organization.Type.Pharmacist, "Pharmacy Org in Pharmaceuitical", "Boston city", 0);
+
+       // System.out.println("network 2 ----> "+net2org1.getSupportedRole());
+        ArrayList<Organization> orgListnet2 = onet2.getOrganizationList();
+//        System.out.println("ORG LIST SIZE " + orgList.size());
+
+        ArrayList<Enterprise> eListnet2 = edirnetwork2.getEnterpriseList();
+        edirnetwork2.setEnterpriseList(eListnet2);
+
+        Employee empnet2 = net2org1.getEmployeeDirectory().createEmployee("Doctor Namratha");
+
+        net2org1.getUserAccountDirectory().createUserAccount("Namu", "Namu", empnet2, net2org1.getSupportedRole().get(0));
+        net2org1.addProduct("Prescription 1 from network 2\n");
+
+        net2org2.getUserAccountDirectory().createUserAccount("Pharmacist Prerana", "prerana", empnet2, net2org2.getSupportedRole().get(0));
+        net2org2.addProduct("Drug 1 from network 2\n");
+
+        n2.setEnterpriseDirectory(edirnetwork2);
+
+        
+        
         for (Network net : system.getNetworks()) {
+           // System.out.println(system.getNetworks());
             for (Enterprise ent : net.getEnterpriseDirectory().getEnterpriseList()) {
                 System.out.println(ent.getEnterpriseType());
                 System.out.println(ent.getOrganizationDirectory().getOrganizationList().size());
-                System.out.println(ent.getEnterpriseUserAccountDirectory().getUserAccountList().get(0).getUsername());
+
 //                if (ent.getEnterpriseType().toString().equals("Vaccine and Immunization")) {
 //                    traverse only through this
+
                 for (Organization or : ent.getOrganizationDirectory().getOrganizationList()) {
                     System.out.println(or.getName() + " *** ");
 
                 }
+
 //                }
 
             }
@@ -171,6 +213,8 @@ public class ConfigureASystem {
 
         
 //        PRINT CUSTOMER ORDERS for that enterprise
+
+
         return system;
     }
 }
--- DoorDeliveryApplication/src/Business/Enterprise/Role/PharmaceuticalEnterpriseAdminRole.java
+++ DoorDeliveryApplication/src/Business/Enterprise/Role/PharmaceuticalEnterpriseAdminRole.java
@@ -9,6 +9,7 @@ import Business.Ecosystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
+import UI.PharmacyEnterpriseRole.PharmaceuticalEnterpriseJPanel;
 import javax.swing.JPanel;
 
 /**
@@ -18,7 +19,7 @@ import javax.swing.JPanel;
 public class PharmaceuticalEnterpriseAdminRole extends EnterpriseRole {
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem business) {
-//        return new DoctorWorkAreaJPanel(userProcessContainer, account, (DoctorOrganization)organization, enterprise); 
-        return null;
+   //  return new PharmaceuticalEnterpriseJPanel(userProcessContainer, account, organization, enterprise); 
+    return null;
     }
 }
